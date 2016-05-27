ff7674dcec36b49b3f5741e65844f34c4a02cf4a
MeMostenes <memo.serrano.82@gmail.com>
2016-05-26 19:42:00 -0500

    Correcion de archivos vacios

--- app/src/main/java/com/memostenes/agenda/Confirma.java
+++ app/src/main/java/com/memostenes/agenda/Confirma.java
@@ -1,7 +1,11 @@
 package com.memostenes.agenda;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Confirma extends AppCompatActivity {
 
@@ -9,5 +13,51 @@ public class Confirma extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirma);
+        Bundle p = getIntent().getExtras();
+        String nombre = p.getString("Nombre");
+        TextView tvNombre2 = (TextView) findViewById(R.id.tvNombre2);
+        tvNombre2.setText(nombre);
+        
+        nombre = p.getString("Dia")+"/"+p.getString("Mes")+"/"+p.getString("Year");
+        TextView tvDFecha = (TextView) findViewById(R.id.tvDFecha);
+        tvDFecha.setText(nombre);
+        nombre = p.getString("Telefono");
+        TextView tvDTel = (TextView) findViewById(R.id.tvDTel);
+        tvDTel.setText(nombre);
+        nombre = p.getString("Correo");
+        TextView tvDMail = (TextView) findViewById(R.id.tvDMail);
+        tvDMail.setText(nombre);
+        nombre = p.getString("Descripcion");
+        TextView tvDDescr = (TextView) findViewById(R.id.tvDDescr);
+        tvDDescr.setText(nombre);
+
+        Button btnEditar = (Button) findViewById(R.id.btnEditar);
+        btnEditar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Confirma.this, MainActivity.class);
+                Bundle p = getIntent().getExtras();
+                String x = p.getString("Nombre");
+                String xx = p.getString("Telefono");
+                String xxx = p.getString("Correo");
+                String xxxx = p.getString("Descripcion");
+                String d = p.getString("Dia");
+                String m = p.getString("Mes");
+                String y = p.getString("Year");
+
+                intent.putExtra("Nombree", x);
+                intent.putExtra("Telefonoo", xx);
+                intent.putExtra("Correoo", xxx);
+                intent.putExtra("Descripcionn", xxxx);
+                //intent.putExtra("Fechaa", fe);
+                intent.putExtra("Dia", d);
+                intent.putExtra("Mes", m);
+                intent.putExtra("Year", y);
+
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 }
--- app/src/main/java/com/memostenes/agenda/MainActivity.java
+++ app/src/main/java/com/memostenes/agenda/MainActivity.java
@@ -1,13 +1,88 @@
 package com.memostenes.agenda;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 
 public class MainActivity extends AppCompatActivity {
-
+    private android.support.design.widget.TextInputEditText n;
+    private android.support.design.widget.TextInputEditText t;
+    private android.support.design.widget.TextInputEditText c;
+    private android.support.design.widget.TextInputEditText d;
+    private DatePicker dp;
+    private String nombre;
+    private boolean sema;
+    private Bundle p;
+    private int dia, mes, year;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //carga los datos anteriores
+
+        if(getIntent().getExtras()!=null) {
+            p = getIntent().getExtras();
+            sema=true;
+        }
+
+
+        Button btnSiguiente = (Button) findViewById(R.id.btnSiguiente);
+
+        n = (android.support.design.widget.TextInputEditText)findViewById(R.id.itNombre);
+        t = (android.support.design.widget.TextInputEditText)findViewById(R.id.tiTel);
+        c = (android.support.design.widget.TextInputEditText)findViewById(R.id.correo);
+        d = (android.support.design.widget.TextInputEditText)findViewById(R.id.descr);
+        dp = (DatePicker) findViewById(R.id.dpFecha);
+       if (sema==true) {
+           nombre = p.getString("Nombree");
+           n.setText(nombre);
+           nombre = p.getString("Telefonoo");
+           t.setText(nombre);
+           nombre = p.getString("Correoo");
+           c.setText(nombre);
+           nombre = p.getString("Descripcionn");
+           d.setText(nombre);
+           String x=p.getString("Dia");
+           dia= Integer.parseInt(x);
+           x=p.getString("Mes");
+           mes = Integer.parseInt(x);
+           mes--;
+           x=p.getString("Year");
+           year = Integer.parseInt(x);
+           //year=p.getInt("Year");
+           //dp.updateDate(year, mes, dia);
+           //dp.updateDate(1996,10,15);
+           dp.updateDate(year,mes,dia);
+        }
+
+        btnSiguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Confirma.class);
+                String x = n.getText().toString();
+                String xx = t.getText().toString();
+                String xxx = c.getText().toString();
+                String xxxx = d.getText().toString();
+                int d = dp.getDayOfMonth();
+                int m = dp.getMonth()+1;
+                int y = dp.getYear();
+
+                //String fe = dp.getDayOfMonth()+"/"+n+"/"+dp.getYear();
+
+                intent.putExtra("Nombre", x);
+                intent.putExtra("Telefono", xx);
+                intent.putExtra("Correo", xxx);
+                intent.putExtra("Descripcion", xxxx);
+                intent.putExtra("Dia", ""+d);
+                intent.putExtra("Mes", ""+m);
+                intent.putExtra("Year", ""+y);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
